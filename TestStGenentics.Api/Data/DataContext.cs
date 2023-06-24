31d4e4de8e4311a9b7812407fe1d610c3b09c1d5
Camilo Andres Pinzón Mora <camilo.pinzon@e-chez.com>
2023-06-24 18:14:14 -0500

    Creacion DB

---
+++ TestStGenentics.Api/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestStGenentics.Shared.Entities;
+
+namespace TestStGenentics.Api.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>  options):base(options)
+        {
+
+        }
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Animal>().HasIndex("Name", "Breed").IsUnique();
+           
+        }
+    }
+}
---
+++ TestStGenentics.Shared/Entities/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestStGenentics.Shared.Entities
+{
+    public class Animal
+    {
+        [Key]
+        public int AnimalId { get; set; }
+
+
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+
+        [MaxLength(100)]
+        public string? Breed { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+
+        [MaxLength(25)]
+        public string? Sex { get; set; }
+
+
+        public decimal Price { get; set; }
+
+
+        [MaxLength(25)]
+        public string? Status { get; set; }
+    }
+}
